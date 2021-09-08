714c21ee0d25dc1c8508b96bb79cc08fe90e7caf
Luiz Salazar <luizsalazar87@gmail.com>
2021-09-07 19:54:24 -0500

    change audio format to m4a

--- youtube-dl-install.sh
+++ youtube-dl-install.sh
@@ -7,4 +7,6 @@ pip install youtube-dl
 
 mkdir ~/bin
 cd ~/bin
-echo "youtube-dl --extract-audio --audio-quality 0 --audio-format mp3 -o \"~/storage/shared/Music/%(playlist_title)s/%(title)s.%(ext)s\" " > termux-url-opener
+
+#echo "youtube-dl --extract-audio --audio-quality 0 --audio-format mp3 -o \"~/storage/shared/Music/%(playlist_title)s/%(title)s.%(ext)s\" " > termux-url-opener
+echo "youtube-dl -f bestaudio[ext=m4a] --embed-thumbnail --add-metadata --metadata-from-title \"%(artist)s - %(title)s\" -o \"~/storage/shared/Music/%(playlist_title)s/%(title)s.%(ext)s\" " > termux-url-opener
